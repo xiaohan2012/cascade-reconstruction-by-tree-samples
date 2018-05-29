6104ea6988d56760d7dc149b803868ee24fb8a06
Han Xiao <han.xiao@aalto.fi>
2018-05-29 06:24:51 +0000

    rm irrelevant files

--- core.py
+++ core.py
@@ -1,80 +1,12 @@
-import numpy as np
-import random
-from scipy.stats import entropy
-
-from linalg_helpers import num_spanning_trees_dense
-from graph_helpers import (contract_graph_by_nodes,
-                           extract_nodes, extract_steiner_tree,
-                           has_vertex, gen_random_spanning_tree,
+from graph_helpers import (extract_steiner_tree,
+                           gen_random_spanning_tree,
                            filter_graph_by_edges,
                            reachable_node_set,
                            swap_end_points)
-from inference import infection_probability
 from tqdm import tqdm
 from random_steiner_tree import random_steiner_tree
 
-# @profile
-def det_score_of_steiner_tree(st, g):
-    """
-    Param:
-    --------
-    st: GraphView, steiner tree
-    
-    Returns:
-    --------
-    returns its score of being sampled by the truncate algorithm
-    
-    the score is computed as the determinant of the contracted graph of g on st.vertices()
-    """
-    cg, weights = contract_graph_by_nodes(g, extract_nodes(st))
-    return num_spanning_trees_dense(cg, weights)
 
-
-def resample(steiner_trees, g, m):
-    """resample the trees with probability proportional to their real score / determinant score
-    m: number of sub-samples to draw
-    """
-    det_scores = [det_score_of_steiner_tree(st, g)
-                  for st in steiner_trees]
-    real_scores = np.exp([-st.num_edges() for st in steiner_trees])
-    sampling_importance = real_scores / np.array(det_scores)
-    sampling_importance /= sampling_importance.sum()
-
-    resampled_ids = np.random.choice(np.arange(len(steiner_trees)), m, replace=False, p=sampling_importance)
-    return [steiner_trees[i] for i in resampled_ids]
-
-# @profile
-def node_occurrence_freq(n, trees):
-    """count how many times node `n` occures in `trees`
-    returns (int, int), (yes it is in, not it's not)
-    """
-    yes, no = 0, 0
-    for t in trees:
-        if isinstance(t, set):
-            if n in t:
-                yes += 1
-            else:
-                no += 1
-        else:
-            if has_vertex(t, n):
-                yes += 1
-            else:
-                no += 1
-    return yes, no
-
-
-def uncertainty_entropy(n, trees):
-    yes, no = node_occurrence_freq(n, trees)
-    p = np.array([yes, no], dtype=np.float32)
-    p /= p.sum()
-    return entropy(p)
-
-
-def uncertainty_count(n, trees):
-    yes, no = node_occurrence_freq(n, trees)
-    return min(yes, no)
-
-# @profile
 def sample_steiner_trees(g, obs,
                          method,
                          n_samples,
@@ -139,81 +71,3 @@ def sample_steiner_trees(g, obs,
         steiner_tree_samples.append(st)
 
     return steiner_tree_samples
-
-
-# @profile
-def uncertainty_scores_old(g, obs,
-                           sampler,
-                           method='count'):
-    """
-    calculate uncertainty scores based on sampled steiner trees
-
-    Args:
-
-    Graph `g`
-    list of int `obs`: list of observed nodes
-    sampler: the tree sampler
-    str `method`: {'count', 'entropy'}
-
-    Returns:
-
-    dict of (int, float): node to uncertainty score
-    """
-    if sampler.is_empty:
-        sampler.fill(obs)
-    
-    if method == 'count':
-        uncert = uncertainty_count
-    elif method == 'entropy':
-        uncert = uncertainty_entropy
-    else:
-        raise ValueError('unknown method')
-    
-    non_obs_nodes = set(extract_nodes(g)) - set(obs)
-    r = {n: uncert(n, sampler.samples)
-         for n in non_obs_nodes}
-    return r
-
-
-def uncertainty_scores(g, obs,
-                       sampler,
-                       error_estimator,
-                       normalize_p=None):
-    """
-    calculate uncertainty scores based on sampled steiner trees
-
-    Args:
-
-    Graph `g`
-    list of int `obs`: list of observed nodes
-    sampler: the tree sampler
-    error_estimator: what does the actual counting
-    str `method`: {'count', 'entropy'}
-
-    Returns:
-
-    dict of (int, float): node to uncertainty score
-    """
-    if sampler.is_empty:
-        sampler.fill(obs)
-
-    p = infection_probability(g, obs, sampler, error_estimator)
-    # print('p', p)
-    non_obs_nodes = set(extract_nodes(g)) - set(obs)
-
-    if normalize_p == 'div_max':
-        mask = np.ones(len(p), dtype=np.bool)
-        mask[obs] = 0
-        p[mask] /= p[mask].max()
-    elif normalize_p is None:
-        pass
-    else:
-        raise ValueError('unrecognized "normalize_p" {}'.format(
-            normalize_p))
-        # print('p (normalized)', p)
-
-    uncert = [entropy([v, 1-v]) for v in p]
-    # print(uncert)
-    r = {n: uncert[n]
-         for n in non_obs_nodes}
-    return r
