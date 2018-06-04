aac7b6d047fe10a97d2a203fd520f2191deaad84
Han Xiao <han.xiao@aalto.fi>
2018-06-04 09:46:56 +0000

    evaluate in parallel

--- eval_helpers.py
+++ eval_helpers.py
@@ -4,6 +4,7 @@ import pickle as pkl
 from tqdm import tqdm
 
 from sklearn.metrics import average_precision_score
+from joblib import Parallel, delayed
 from glob import glob
 
 
@@ -40,15 +41,30 @@ def evaluate_edge_prediction(g, true_edges, pred_edge_freq, eval_func):
     return eval_func(edge_true_vect.a, edge_pred_vect.a)
 
 
-def evaluate_edge_in_batch(g, input_dir, output_dir, score_func):
-    scores = []
-    for input_path in tqdm(glob(input_dir + '*.pkl')):
-        basename = os.path.basename(input_path)
-        output_path = os.path.join(output_dir, basename)
+def eval_edge_parallel_task(g, input_path, output_dir, score_func):
+    """one task to send to joblib.Parallel
+    """
+    basename = os.path.basename(input_path)
+    output_path = os.path.join(output_dir, basename)
+    
+    _, _, true_edges = pkl.load(open(input_path, 'rb'))
+    edge_freq = pkl.load(open(output_path, 'rb'))['edge_freq']
+    return evaluate_edge_prediction(g, true_edges, edge_freq, score_func)
+    
 
-        _, _, true_edges = pkl.load(open(input_path, 'rb'))
-        edge_freq = pkl.load(open(output_path, 'rb'))['edge_freq']
-        scores.append(evaluate_edge_prediction(g, true_edges, edge_freq, score_func))
+def evaluate_edge_in_batch(g, input_dir, output_dir, score_func):
+    scores = Parallel(n_jobs=-1)(
+        delayed(eval_edge_parallel_task)(g, input_path, output_dir, score_func)
+        for input_path in tqdm(glob(input_dir + '*.pkl'))
+    )
+    
+    # for input_path in tqdm(glob(input_dir + '*.pkl')):
+    #     basename = os.path.basename(input_path)
+    #     output_path = os.path.join(output_dir, basename)
+
+    #     _, _, true_edges = pkl.load(open(input_path, 'rb'))
+    #     edge_freq = pkl.load(open(output_path, 'rb'))['edge_freq']
+    #     scores.append(evaluate_edge_prediction(g, true_edges, edge_freq, score_func))
 
     return scores
 
